6275bc9c9bba94f043e85e702d37251aba7d0fea
chko182 <chko.martinez@gmail.com>
2015-12-22 19:33:35 -0600

    Update HWPHello.m

--- src/ios/HWPHello.m
+++ src/ios/HWPHello.m
@@ -9,11 +9,39 @@
     NSString* name = [[command arguments] objectAtIndex:0];
     NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
 
+    Boolean bReturn = FALSE;
+	struct ifaddrs *interfaces = NULL;
+	struct ifaddrs *temp_addr = NULL;
+	int success = 0;
+	
+	// retrieve the current interfaces - returns 0 on success
+	success = getifaddrs(&interfaces);
+	
+	if (success == 0) {
+		// Loop through linked list of interfaces
+		temp_addr = interfaces;
+			while(temp_addr != NULL) {
+				if(temp_addr->ifa_addr->sa_family == AF_INET) {
+					// Check if interface is en0 which is the wifi connection on the iPhone
+					if([[NSString stringWithUTF8String:temp_addr->ifa_name] containsString:@"utun"]) {
+						bReturn = true;
+					}
+				}
+				temp_addr = temp_addr->ifa_next;
+			}
+		}
+	
+	// Free memory
+	freeifaddrs(interfaces);
+    
+    if (bReturn)
+        msg = [NSString stringWithUTF8String:"Verdadero"];
+    else
+        msg = [NSString stringWithUTF8String:"Falso"];
+	        
     CDVPluginResult* result = [CDVPluginResult
                                resultWithStatus:CDVCommandStatus_OK
                                messageAsString:msg];
-
-    NSLog(@"Amit Hello");
 
     [self success:result callbackId:callbackId];
 
