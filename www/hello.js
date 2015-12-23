35d2321162dd03cdc456b49350b8e79428a7ac16
chko182 <chko.martinez@gmail.com>
2015-12-22 18:27:23 -0600

    Update hello.js

--- www/hello.js
+++ www/hello.js
@@ -3,5 +3,11 @@
 module.exports = {
     greet: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
-    }
+    },
+	check: function (name, successCallback, errorCallback) {
+		cordova.exec(successCallback, errorCallback, "Hello", "check", [name]);
+	},
+	start: function (name, successCallback, errorCallback) {
+		cordova.exec(successCallback, errorCallback, "Hello", "start", (typeof name === 'string') ? [name] : name);
+	}
 };
